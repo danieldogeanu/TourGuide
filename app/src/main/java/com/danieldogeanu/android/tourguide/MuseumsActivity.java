26cab6e4c548498020fede37b39be25b98c62638
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-23 21:16:59 +0200

    Set Category Title Automatically

--- app/src/main/java/com/danieldogeanu/android/tourguide/MuseumsActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/MuseumsActivity.java
@@ -3,6 +3,7 @@ package com.danieldogeanu.android.tourguide;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MuseumsActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MuseumsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        // Set Category Title
+        TextView categoryTitle = (TextView) findViewById(R.id.category_title);
+        categoryTitle.setText(getText(R.string.museums));
     }
 }
