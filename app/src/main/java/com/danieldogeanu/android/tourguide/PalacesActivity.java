701a90bc1c27c44bc73d56fd467debf558b67049
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-24 12:08:18 +0200

    Added Back Button Functionality for Categories

--- app/src/main/java/com/danieldogeanu/android/tourguide/PalacesActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/PalacesActivity.java
@@ -3,6 +3,8 @@ package com.danieldogeanu.android.tourguide;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class PalacesActivity extends AppCompatActivity {
@@ -15,5 +17,14 @@ public class PalacesActivity extends AppCompatActivity {
         // Set Category Title
         TextView categoryTitle = (TextView) findViewById(R.id.category_title);
         categoryTitle.setText(getText(R.string.palaces));
+
+        // Add Back (Up) Functionality
+        ImageButton backButton = (ImageButton) findViewById(R.id.category_back_btn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
