f6f423d7dea6fabbefde11faa91142f8c09e2646
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-25 23:50:52 +0200

    Changed Placeholder Landmark Names and Added More Images

--- app/src/main/java/com/danieldogeanu/android/tourguide/MonumentsActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/MonumentsActivity.java
@@ -32,16 +32,11 @@ public class MonumentsActivity extends AppCompatActivity {
 
         ArrayList<Landmark> monuments = new ArrayList<>();
 
+        monuments.add(new Landmark("Herastrau Park", "Bucharest", R.drawable.herastrau_park));
+        monuments.add(new Landmark("Palace of the Parliament", "Strada Izvor 2-4, Bucharest", R.drawable.casa_poporului));
         monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
-        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum));
+        monuments.add(new Landmark("National Military Museum", "Strada Mircea Vulcănescu 125-127, Bucharest", R.drawable.military_museum));
+        monuments.add(new Landmark("Museum of the Romanian Railways", "Strada Gării de Nord, Bucharest", R.drawable.railroad_museum));
 
         ListView listView = (ListView) findViewById(R.id.cat_items_list);
         LandmarkAdapter adapter = new LandmarkAdapter(MonumentsActivity.this, monuments, listView);
