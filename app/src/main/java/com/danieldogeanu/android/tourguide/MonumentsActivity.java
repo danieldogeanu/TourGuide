981d0b90b01914862894d9e2dbc30382566c16c1
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-25 16:41:40 +0200

    Populated Monuments Activity with Placeholder Landmark Items

--- app/src/main/java/com/danieldogeanu/android/tourguide/MonumentsActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/MonumentsActivity.java
@@ -5,7 +5,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MonumentsActivity extends AppCompatActivity {
 
@@ -26,5 +29,25 @@ public class MonumentsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        ArrayList<Landmark> monuments = new ArrayList<>();
+
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentPink));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentAmber));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentBlue));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentGreen));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentDeepOrange));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentCyan));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentPurple));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentBrown));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentTeal));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentLime));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentRed));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentLightGreen));
+        monuments.add(new Landmark("National Museum of Romanian History", "Calea Victoriei 12, Bucharest", R.drawable.history_museum, R.color.TransparentDeepPurple));
+
+        ListView listView = (ListView) findViewById(R.id.cat_items_list);
+        LandmarkAdapter adapter = new LandmarkAdapter(MonumentsActivity.this, monuments, listView);
+        listView.setAdapter(adapter);
     }
 }
