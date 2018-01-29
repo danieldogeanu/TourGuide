e70e8b7c054aae23f2a57dfde8dda7d028f1c057
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-29 12:55:35 +0200

    Added Back Up Functionality in Detail Activity

--- app/src/main/java/com/danieldogeanu/android/tourguide/DetailActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/DetailActivity.java
@@ -3,11 +3,22 @@ package com.danieldogeanu.android.tourguide;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // Add Back (Up) Functionality
+        ImageButton backButton = (ImageButton) findViewById(R.id.detail_back_btn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
