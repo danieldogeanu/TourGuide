7cfb7de16f7308fa1f2ec2e8c1c5ff4ca1056479
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-29 22:41:08 +0200

    Populated Palaces Activity with Data

--- app/src/main/java/com/danieldogeanu/android/tourguide/PalacesActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/PalacesActivity.java
@@ -1,11 +1,16 @@
 package com.danieldogeanu.android.tourguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PalacesActivity extends AppCompatActivity {
 
@@ -24,6 +29,49 @@ public class PalacesActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        // Get String Array Resources
+        String[] palacesNames = getResources().getStringArray(R.array.palaces_names);
+        String[] palacesAdresses = getResources().getStringArray(R.array.palaces_addresses);
+        String[] palacesDescriptions = getResources().getStringArray(R.array.palaces_descriptions);
+        String[] palacesHours = getResources().getStringArray(R.array.palaces_hours);
+        String[] palacesPhones = getResources().getStringArray(R.array.palaces_phones);
+        int[] palacesImages = {};
+
+        // Initialize ArrayList of Landmarks
+        final ArrayList<Landmark> palaces = new ArrayList<>();
+
+        // Add All Landmarks to the ArrayList
+        for (int i = 0; i < palacesNames.length; i++) {
+            palaces.add(new Landmark(
+                    palacesNames[i],
+                    palacesDescriptions[i],
+                    palacesAdresses[i],
+                    palacesHours[i],
+                    "+40 " + palacesPhones[i],
+                    R.drawable.history_museum));
+        }
+
+        // Set Custom List View Adapter
+        ListView listView = (ListView) findViewById(R.id.cat_items_list);
+        LandmarkAdapter adapter = new LandmarkAdapter(PalacesActivity.this, palaces, listView);
+        listView.setAdapter(adapter);
+
+        // Set Click Listeners for Each Item
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // Get Landmark object at current position
+                Landmark palace = palaces.get(position);
+
+                // Start intent and send Landmark object to DetailActivity
+                Intent detailActivity = new Intent(PalacesActivity.this, DetailActivity.class);
+                detailActivity.putExtra("serialize_data", palace);
+                startActivity(detailActivity);
+
             }
         });
     }
