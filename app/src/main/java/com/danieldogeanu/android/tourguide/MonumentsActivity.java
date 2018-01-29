20d639ce4ec9a6dc07bbad6fddd9ad9ff9eb6ae0
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-29 22:32:36 +0200

    Removed Test Code From Monuments Activity

--- app/src/main/java/com/danieldogeanu/android/tourguide/MonumentsActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/MonumentsActivity.java
@@ -1,16 +1,11 @@
 package com.danieldogeanu.android.tourguide;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MonumentsActivity extends AppCompatActivity {
 
@@ -29,63 +24,6 @@ public class MonumentsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
-            }
-        });
-
-        final ArrayList<Landmark> monuments = new ArrayList<>();
-
-        monuments.add(new Landmark(
-                "Herastrau Park",
-                "Herăstrău Park is a large park on the northern side of Bucharest, Romania, around Lake Herăstrău, one of the lakes formed by the Colentina River.",
-                "Bucharest",
-                "Open 24/7",
-                "",
-                R.drawable.herastrau_park));
-        monuments.add(new Landmark(
-                "Palace of the Parliament",
-                "",
-                "Strada Izvor 2-4, Bucharest",
-                "",
-                "",
-                R.drawable.casa_poporului));
-        monuments.add(new Landmark(
-                "National Museum of Romanian History",
-                "",
-                "Calea Victoriei 12, Bucharest",
-                "",
-                "",
-                R.drawable.history_museum));
-        monuments.add(new Landmark(
-                "National Military Museum",
-                "",
-                "Strada Mircea Vulcănescu 125-127, Bucharest",
-                "",
-                "",
-                R.drawable.military_museum));
-        monuments.add(new Landmark(
-                "Museum of the Romanian Railways",
-                "",
-                "Strada Gării de Nord, Bucharest",
-                "",
-                "",
-                R.drawable.railroad_museum));
-
-        ListView listView = (ListView) findViewById(R.id.cat_items_list);
-        LandmarkAdapter adapter = new LandmarkAdapter(MonumentsActivity.this, monuments, listView);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                // Get Landmark object at current position
-                Landmark monument = monuments.get(position);
-
-                // Start intent and send Landmark object to DetailActivity
-                Intent detailActivity = new Intent(MonumentsActivity.this, DetailActivity.class);
-                detailActivity.putExtra("serialize_data", monument);
-                startActivity(detailActivity);
-
             }
         });
     }
