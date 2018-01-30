fde43ca52e3fa1e267f4526018841b42726b7374
Daniel Dogeanu <contact@danieldogeanu.com>
2018-01-30 17:59:00 +0200

    Populated Monuments Activity with Data

--- app/src/main/java/com/danieldogeanu/android/tourguide/MonumentsActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/MonumentsActivity.java
@@ -1,11 +1,16 @@
 package com.danieldogeanu.android.tourguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MonumentsActivity extends AppCompatActivity {
 
@@ -24,6 +29,49 @@ public class MonumentsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        // Get String Array Resources
+        String[] monumentsNames = getResources().getStringArray(R.array.monuments_names);
+        String[] monumentsAdresses = getResources().getStringArray(R.array.monuments_addresses);
+        String[] monumentsDescriptions = getResources().getStringArray(R.array.monuments_descriptions);
+        String[] monumentsHours = getResources().getStringArray(R.array.monuments_hours);
+        String[] monumentsPhones = getResources().getStringArray(R.array.monuments_phones);
+        int[] monumentsImages = {};
+
+        // Initialize ArrayList of Landmarks
+        final ArrayList<Landmark> monuments = new ArrayList<>();
+
+        // Add All Landmarks to the ArrayList
+        for (int i = 0; i < monumentsNames.length; i++) {
+            monuments.add(new Landmark(
+                    monumentsNames[i],
+                    monumentsDescriptions[i],
+                    monumentsAdresses[i],
+                    monumentsHours[i],
+                    "+40 " + monumentsPhones[i],
+                    R.drawable.history_museum));
+        }
+
+        // Set Custom List View Adapter
+        ListView listView = (ListView) findViewById(R.id.cat_items_list);
+        LandmarkAdapter adapter = new LandmarkAdapter(MonumentsActivity.this, monuments, listView);
+        listView.setAdapter(adapter);
+
+        // Set Click Listeners for Each Item
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // Get Landmark object at current position
+                Landmark monument = monuments.get(position);
+
+                // Start intent and send Landmark object to DetailActivity
+                Intent detailActivity = new Intent(MonumentsActivity.this, DetailActivity.class);
+                detailActivity.putExtra("serialize_data", monument);
+                startActivity(detailActivity);
+
             }
         });
     }
