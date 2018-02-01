edc28fa1153063636500d2e48c6c67fbb8ee7600
Daniel Dogeanu <contact@danieldogeanu.com>
2018-02-01 07:19:02 +0200

    Added Array with Parks Map URIs

--- app/src/main/java/com/danieldogeanu/android/tourguide/ParksActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/ParksActivity.java
@@ -38,8 +38,17 @@ public class ParksActivity extends AppCompatActivity {
         String[] parksDescriptions = getResources().getStringArray(R.array.parks_descriptions);
         String[] parksHours = getResources().getStringArray(R.array.parks_hours);
         String[] parksPhones = getResources().getStringArray(R.array.parks_phones);
-//        String[] parksMapUris = {}; // TODO: Add String Array of Map URIs for Parks
-        String mapUri = "https://www.google.com/maps/dir//Palace+of+Parliament,+Strada+Izvor+2-4,+Bucure%C8%99ti/@44.4275035,26.0873506,15z/data=!4m16!1m6!3m5!1s0x0:0x2b1089f802abaddc!2sPalace+of+Parliament!8m2!3d44.4275035!4d26.0873506!4m8!1m0!1m5!1m1!1s0x40b1ff427bee28c1:0x2b1089f802abaddc!2m2!1d26.0873506!2d44.4275035!3e3";
+        String[] parksMapUris = {
+                "https://www.google.com/maps/dir//Zoo+Bucharest,+Strada+Vadul+Moldovei+4,+Bucure%C8%99ti+077190/@44.5172694,26.1044899,15z/data=!4m16!1m6!3m5!1s0x0:0x23f987bbc405cd2a!2sZoo+Bucharest!8m2!3d44.5172694!4d26.1044899!4m8!1m0!1m5!1m1!1s0x40b202dab1f665f5:0x23f987bbc405cd2a!2m2!1d26.1044899!2d44.5172694!3e3",
+                "https://www.google.com/maps/dir//King+Michael+I+Park,+Bucure%C8%99ti/@44.4702015,26.0827527,15z/data=!4m16!1m6!3m5!1s0x0:0x57f60ea89ec15526!2sKing+Michael+I+Park!8m2!3d44.4702015!4d26.0827527!4m8!1m0!1m5!1m1!1s0x40b20213bb079091:0x57f60ea89ec15526!2m2!1d26.0827527!2d44.4702015!3e3",
+                "https://www.google.com/maps/dir//Ci%C8%99migiu+Park,+Bulevardul+Regina+Elisabeta,+Bucure%C8%99ti+030167/@44.4369687,26.0909837,15z/data=!4m16!1m6!3m5!1s0x0:0x3e98ab275166423b!2sCi%C8%99migiu+Park!8m2!3d44.4369687!4d26.0909837!4m8!1m0!1m5!1m1!1s0x40b1ff438f7e6fcf:0x3e98ab275166423b!2m2!1d26.0909837!2d44.4369687!3e3",
+                "https://www.google.com/maps/dir//Youth+Park,+Bucure%C8%99ti/@44.4004916,26.1084371,15z/data=!4m16!1m6!3m5!1s0x0:0x9c229d7528e44f8e!2sYouth+Park!8m2!3d44.4004916!4d26.1084371!4m8!1m0!1m5!1m1!1s0x40b1ffaad2d5dcff:0x9c229d7528e44f8e!2m2!1d26.1084371!2d44.4004916!3e3",
+                "https://www.google.com/maps/dir//Alexandru+Ioan+Cuza+Park,+Bucure%C8%99ti/@44.4255117,26.1535332,15z/data=!4m16!1m6!3m5!1s0x0:0xf0489031c3cf5127!2sAlexandru+Ioan+Cuza+Park!8m2!3d44.4255117!4d26.1535332!4m8!1m0!1m5!1m1!1s0x40b1feb663d02dd3:0xf0489031c3cf5127!2m2!1d26.1535332!2d44.4255117!3e3",
+                "https://www.google.com/maps/dir//Carol+Park,+Bulevardul+M%C4%83r%C4%83%C8%99e%C8%99ti,+Bucure%C8%99ti/@44.4186655,26.0965164,15z/data=!4m16!1m6!3m5!1s0x0:0xbd8b38c1281359f0!2sCarol+Park!8m2!3d44.4186655!4d26.0965164!4m8!1m0!1m5!1m1!1s0x40b1ff0e5e8c275b:0xbd8b38c1281359f0!2m2!1d26.0965164!2d44.4186655!3e3",
+                "https://www.google.com/maps/dir//Drumul+Taberei+Park,+Drumul+Taberei,+Bucure%C8%99ti/@44.4208643,26.0313024,15z/data=!4m16!1m6!3m5!1s0x0:0x8772154e0f9b91ea!2sDrumul+Taberei+Park!8m2!3d44.4208643!4d26.0313024!4m8!1m0!1m5!1m1!1s0x40b20036c2aff6b1:0x8772154e0f9b91ea!2m2!1d26.0313024!2d44.4208643!3e3",
+                "https://www.google.com/maps/dir//Botanic+Garden,+%C8%98oseaua+Cotroceni+32,+Bucure%C8%99ti/@44.4372286,26.0626771,15z/data=!4m16!1m6!3m5!1s0x0:0x301135551489fb73!2sBotanic+Garden!8m2!3d44.4372286!4d26.0626771!4m8!1m0!1m5!1m1!1s0x40b201dd97d58297:0x301135551489fb73!2m2!1d26.0626771!2d44.4372286!3e3",
+                "https://www.google.com/maps/dir//Kiseleff+Park,+Bucure%C8%99ti/@44.4576517,26.0836661,15z/data=!4m16!1m6!3m5!1s0x0:0x2fe176e5d7d1b1ca!2sKiseleff+Park!8m2!3d44.4576517!4d26.0836661!4m8!1m0!1m5!1m1!1s0x40b20203c31f6dab:0x2fe176e5d7d1b1ca!2m2!1d26.0836661!2d44.4576517!3e3",
+        };
         int[] parksImages = {
                 R.drawable.baneasa_zoo,
                 R.drawable.herastrau_park,
@@ -63,8 +72,7 @@ public class ParksActivity extends AppCompatActivity {
                     parksAdresses[i],
                     parksHours[i],
                     addPrefix(parksPhones[i]),
-//                    parksMapUris[i],
-                    mapUri,
+                    parksMapUris[i],
                     parksImages[i]));
         }
 
