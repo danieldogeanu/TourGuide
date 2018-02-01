1ebdc473c9f8d66987cacb610576348451cacb24
Daniel Dogeanu <contact@danieldogeanu.com>
2018-02-01 07:08:19 +0200

    Added Array with Museums Map URIs

--- app/src/main/java/com/danieldogeanu/android/tourguide/MuseumsActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/MuseumsActivity.java
@@ -38,8 +38,18 @@ public class MuseumsActivity extends AppCompatActivity {
         String[] museumsDescriptions = getResources().getStringArray(R.array.museums_descriptions);
         String[] museumsHours = getResources().getStringArray(R.array.museums_hours);
         String[] museumsPhones = getResources().getStringArray(R.array.museums_phones);
-//        String[] museumsMapUris = {}; // TODO: Add String Array of Map URIs for Museums
-        String mapUri = "https://www.google.com/maps/dir//Palace+of+Parliament,+Strada+Izvor+2-4,+Bucure%C8%99ti/@44.4275035,26.0873506,15z/data=!4m16!1m6!3m5!1s0x0:0x2b1089f802abaddc!2sPalace+of+Parliament!8m2!3d44.4275035!4d26.0873506!4m8!1m0!1m5!1m1!1s0x40b1ff427bee28c1:0x2b1089f802abaddc!2m2!1d26.0873506!2d44.4275035!3e3";
+        String[] museumsMapUris = {
+                "https://www.google.com/maps/dir//The+National+Museum+of+Art+of+Romania,+Calea+Victoriei+49-53,+Bucure%C8%99ti+010063/@44.4393668,26.095874,15z/data=!4m16!1m6!3m5!1s0x0:0xdea7c86f153f2330!2sThe+National+Museum+of+Art+of+Romania!8m2!3d44.4393668!4d26.095874!4m8!1m0!1m5!1m1!1s0x40b1ff45a2c1486f:0xdea7c86f153f2330!2m2!1d26.095874!2d44.4393668!3e3",
+                "https://www.google.com/maps/dir//Muzeul+Satului+Dimitrie+Gusti,+%C8%98oseaua+Pavel+Dimitrievici+Kiseleff,+sector+1+28-30,+Bucure%C8%99ti+011347/@44.4723586,26.0765852,15z/data=!4m16!1m6!3m5!1s0x0:0x82df7091a7a741a3!2sMuzeul+Satului+Dimitrie+Gusti!8m2!3d44.4723586!4d26.0765852!4m8!1m0!1m5!1m1!1s0x40b202037c613773:0x82df7091a7a741a3!2m2!1d26.0765852!2d44.4723586!3e3",
+                "https://www.google.com/maps/dir//Antipa+Museum,+%C8%98oseaua+Pavel+Dimitrievici+Kiseleff+1,+Bucure%C8%99ti+011341/@44.4531131,26.0846382,15z/data=!4m16!1m6!3m5!1s0x0:0x120b912fe44ec81d!2sAntipa+Museum!8m2!3d44.4531131!4d26.0846382!4m8!1m0!1m5!1m1!1s0x40b201ff7eb6eee5:0x120b912fe44ec81d!2m2!1d26.0846382!2d44.4531131!3e3",
+                "https://www.google.com/maps/dir//Museum+of+the+Romanian+Peasant,+%C8%98oseaua+Pavel+Dimitrievici+Kiseleff+3,+Bucure%C8%99ti+011341/@44.4544198,26.0836002,15z/data=!4m16!1m6!3m5!1s0x0:0xddcebd3aef3114e3!2sMuseum+of+the+Romanian+Peasant!8m2!3d44.4544198!4d26.0836002!4m8!1m0!1m5!1m1!1s0x40b20201893a4cd1:0xddcebd3aef3114e3!2m2!1d26.0836002!2d44.4544198!3e3",
+                "https://www.google.com/maps/dir//National+Museum+of+Romanian+History,+Calea+Victoriei+12,+Bucure%C8%99ti+030026/@44.431447,26.0974538,15z/data=!4m16!1m6!3m5!1s0x0:0x50934f855fca87e9!2sNational+Museum+of+Romanian+History!8m2!3d44.431447!4d26.0974538!4m8!1m0!1m5!1m1!1s0x40b1ff40166ffb91:0x50934f855fca87e9!2m2!1d26.0974538!2d44.431447!3e3",
+                "https://www.google.com/maps/dir//Curtea+Veche,+Strada+Francez%C4%83+25,+Bucure%C8%99ti+030167/@44.4301472,26.1009639,15z/data=!4m16!1m6!3m5!1s0x0:0xd53f908e1cf2db66!2sCurtea+Veche!8m2!3d44.4301472!4d26.1009639!4m8!1m0!1m5!1m1!1s0x40b1ff3fd305aeed:0xd53f908e1cf2db66!2m2!1d26.1009639!2d44.4301472!3e3",
+                "https://www.google.com/maps/dir//National+Museum+of+Contemporary+Art,+Bucure%C8%99ti+030167/@44.4281238,26.0865004,15z/data=!4m16!1m6!3m5!1s0x0:0xb4b9a8d0644c2ca!2sNational+Museum+of+Contemporary+Art!8m2!3d44.4281238!4d26.0865004!4m8!1m0!1m5!1m1!1s0x40b1ff6e98722e47:0xb4b9a8d0644c2ca!2m2!1d26.0865004!2d44.4281238!3e3",
+                "https://www.google.com/maps/dir//Templul+Unirea+Sf%C3%A2nt%C4%83,+Intrarea+M%C4%83mulari+3,+Bucure%C8%99ti+030167/@44.4283435,26.1080229,15z/data=!4m16!1m6!3m5!1s0x0:0x95a373390ee74fe!2sTemplul+Unirea+Sf%C3%A2nt%C4%83!8m2!3d44.4283435!4d26.1080229!4m8!1m0!1m5!1m1!1s0x40b1ff3d795bfb25:0x95a373390ee74fe!2m2!1d26.1080229!2d44.4283435!3e3",
+                "https://www.google.com/maps/dir//National+Military+Museum,+Strada+Mircea+Vulc%C4%83nescu+125-127,+Bucure%C8%99ti+010819/@44.4407516,26.076598,15z/data=!4m16!1m6!3m5!1s0x0:0x7a32aefdf0d8b116!2sNational+Military+Museum!8m2!3d44.4407516!4d26.076598!4m8!1m0!1m5!1m1!1s0x40b201e2102a641b:0x7a32aefdf0d8b116!2m2!1d26.076598!2d44.4407516!3e3",
+                "https://www.google.com/maps/dir//Muzeu+CFR,+Strada+G%C4%83rii+de+Nord,+Bucure%C8%99ti/@44.447968,26.0731395,15z/data=!4m16!1m6!3m5!1s0x0:0x376e0682d84587c1!2sMuzeu+CFR!8m2!3d44.447968!4d26.0731395!4m8!1m0!1m5!1m1!1s0x40b201fb1a0e7f69:0x376e0682d84587c1!2m2!1d26.0731395!2d44.447968!3e3"
+        };
         int[] museumsImages = {
                 R.drawable.national_art_museum_of_romania,
                 R.drawable.dimitrie_gusti_national_village_museum,
@@ -64,8 +74,7 @@ public class MuseumsActivity extends AppCompatActivity {
                     museumsAdresses[i],
                     museumsHours[i],
                     addPrefix(museumsPhones[i]),
-//                    museumsMapUris[i],
-                    mapUri,
+                    museumsMapUris[i],
                     museumsImages[i]));
         }
 
