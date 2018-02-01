721be4183e09f8502fb0086c8c63a1184985ecf0
Daniel Dogeanu <contact@danieldogeanu.com>
2018-02-01 05:48:48 +0200

    Added Intent to Open Google Maps for Directions

--- app/src/main/java/com/danieldogeanu/android/tourguide/DetailActivity.java
+++ app/src/main/java/com/danieldogeanu/android/tourguide/DetailActivity.java
@@ -1,14 +1,21 @@
 package com.danieldogeanu.android.tourguide;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private static final int CALL = 243247;
+    private static final int MAPS = 892343;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +48,10 @@ public class DetailActivity extends AppCompatActivity {
         // Set Address
         if (!thisLandmark.getAddress().isEmpty()) {
             fillText(R.id.details_address_content, thisLandmark.getAddress());
+
+            // Attach Intent to Get Directions Button
+            String mapUri = "https://www.google.com/maps/dir//Palace+of+Parliament,+Strada+Izvor+2-4,+Bucure%C8%99ti/@44.4275035,26.0873506,15z/data=!4m16!1m6!3m5!1s0x0:0x2b1089f802abaddc!2sPalace+of+Parliament!8m2!3d44.4275035!4d26.0873506!4m8!1m0!1m5!1m1!1s0x40b1ff427bee28c1:0x2b1089f802abaddc!2m2!1d26.0873506!2d44.4275035!3e3";
+            attachClickListener(R.id.detail_directions_btn, MAPS, mapUri);
         } else {
             hideView(R.id.details_address_container);
         }
@@ -59,6 +70,7 @@ public class DetailActivity extends AppCompatActivity {
             hideView(R.id.details_phone_container);
             hideView(R.id.detail_call_btn_frame);
         }
+
     }
 
     private void fillText(int id, String text) {
@@ -74,6 +86,27 @@ public class DetailActivity extends AppCompatActivity {
     private void hideView(int id) {
         View thisView = (View) findViewById(id);
         thisView.setVisibility(View.GONE);
+    }
+
+    private void attachClickListener(int buttonId, final int action, final String data) {
+        Button button = (Button) findViewById(buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (action == MAPS) {
+                    launchMaps(data);
+                }
+            }
+        });
+    }
+
+    private void launchMaps(String uri) {
+        Uri gmIntentUri = Uri.parse(uri);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
     }
 
 }
